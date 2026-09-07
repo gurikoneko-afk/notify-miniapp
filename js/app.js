602c80a770ddd662270d2fc7cb1201ac1e7def5d
gurikoneko-afk <gurikoneko@gmail.com>
2026-09-07 17:42:26 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -284,7 +284,7 @@ function renderRoute() {
   const button = createElement(
     'button',
     'retry-button',
-    '池田エライザでテスト'
+    '矢田 萌華でテスト'
   );
 
   const result = createElement(
@@ -301,7 +301,7 @@ function renderRoute() {
       const response = await NotifyApi.call(
         'favorite.resolve',
         {
-          input: '池田エライザ'
+          input: '矢田 萌華'
         }
       );
 
